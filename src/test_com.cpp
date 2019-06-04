1167e3c55949c054f6d44cbf6227dc3ac79b510f
AshayAswale <asaswale@wpi.edu>
2019-06-04 12:29:11 -0400

    Modified the COM updating in RVIZ

--- src/test_com.cpp
+++ src/test_com.cpp
@@ -20,20 +20,14 @@ int main(int argc, char **argv)
   tf::Point com_wrt_root;
   double mass_robot = 181.82;
 
-  // while(joint_state.name.size() == 0)
   ros::Duration(1.0).sleep();
 
-  robot_state->getJointStateMessage(joint_state);
-  for (std::size_t i = 0; i < joint_state.name.size(); i++)
-  {
-    joint_positions.insert({joint_state.name[i], joint_state.position[i]});
-  }
+  std::vector<std::string> left_arm_names, chest_names;
+  rd->getLeftArmJointNames(left_arm_names);
+  rd->getChestJointNames(chest_names);
 
   tf::Transform tf_right_foot, tf_left_foot;
   com COM(nh, rd->getPelvisFrame(), rd->getRightFootFrameName(), rd->getLeftFootFrameName(), true);
-  COM.computeCOM(joint_positions, com_wrt_root, mass_robot, tf_right_foot, tf_left_foot);
-
-  ROS_INFO_STREAM("Center of Mass WRT pelvis: " << com_wrt_root.getX()<<", "<< com_wrt_root.getY()<<", "<<com_wrt_root.getZ());
 
   geometry_msgs::Polygon polygon_points;
   geometry_msgs::PolygonStamped polygon_points_stamp;
@@ -41,19 +35,31 @@ int main(int argc, char **argv)
   geometry_msgs::PointStamped point_stamp;
   point_stamp.header.frame_id = rd->getPelvisFrame();
   polygon_points_stamp.header.frame_id = rd->getWorldFrame();
-  // point_sta
-  tf::pointTFToMsg(com_wrt_root, point_geom);
-    point_stamp.point = point_geom;
-    com_publisher.publish(point_stamp);
 
   while (ros::ok())
   {
+    joint_positions.clear();
+    ros::Duration(0.2).sleep();
+    robot_state->getJointStateMessage(joint_state);
+    for (std::size_t i = 0; i < joint_state.name.size(); i++)
+    {
+      // if(std::find(chest_names.begin(), chest_names.end(), joint_state.name[i]) != chest_names.end())
+      //   joint_positions.insert({ joint_state.name[i], 0.34 });
+      // else
+      joint_positions.insert({ joint_state.name[i], joint_state.position[i] });
+    }
+
+    COM.computeCOM(joint_positions, com_wrt_root, mass_robot, tf_right_foot, tf_left_foot);
+    tf::pointTFToMsg(com_wrt_root, point_geom);
+    point_stamp.point = point_geom;
+    point_stamp.point.z = -0.78;
+    com_publisher.publish(point_stamp);
+
     robot_state->getSupportPolygon(polygon_points);
-    // ROS_INFO_STREAM_THROTTLE(0.1,polygon_points);
     polygon_points_stamp.polygon = polygon_points;
     polygon_publisher.publish(polygon_points_stamp);
     bool point_in = robot_state->isPointInSupportPolygon(point_geom);
-    // ROS_INFO_STREAM("####  COM IN STATUS  ###"<<point_in);
+    ROS_INFO_STREAM("COM IN STATUS  "<<point_in);
   }
   return 0;
 }
