5535d85696bb19590a0f4eb74228e145126b6b1c
AshayAswale <asaswale@wpi.edu>
2019-09-28 21:27:25 -0400

    WIP - left shoulder working

--- include/imitation_game/shadow_upper_body.h
+++ include/imitation_game/shadow_upper_body.h
@@ -17,7 +17,7 @@ private:
   std::map<std::string, int> jointIndexMap();
   trajectory_msgs::JointTrajectory joint_trajectory_;
 
-  double time_execution = 0.01;
+  double time_execution = 0.05;
   bool run_code = true;
 
   std::string OPNNI_PREFIX_ = "openni/";
@@ -31,23 +31,33 @@ private:
               left_elbow_yaw_ = "left_elbow_yaw", left_elbow_roll_ = "left_elbow_roll",
               right_elbow_yaw_ = "right_elbow_yaw", right_elbow_roll_ = "right_elbow_roll";
 
-  std::string yaw_ = "yaw", roll_ = "roll";
+  std::string yaw_ = "_yaw", roll_ = "_roll";
 
   std::map<std::string, std::string> human_robot_joint_map_ = {
     { left_shoulder_frame_+ yaw_, "l_arm_shz" },  { left_shoulder_frame_+ roll_, "l_arm_shx" },
     { right_shoulder_frame_+ yaw_, "r_arm_shz" }, { right_shoulder_frame_+ roll_, "r_arm_shx" },
-    { left_elbow_frame_+ yaw_, "l_arm_elz" },     { left_elbow_frame_+ roll_, "l_arm_elx" },
+    { left_elbow_frame_+ yaw_, "l_arm_ely" },     { left_elbow_frame_+ roll_, "l_arm_elx" },
     { right_elbow_frame_+ yaw_, "r_arm_elz" },    { right_elbow_frame_+ roll_, "r_arm_elx" }
   };
 
+  std::map<std::string, std::string>::iterator string_map_iterator_;
   std::map<std::string, std::string> child_parent_frames_ = { { right_elbow_frame_, right_shoulder_frame_ },
                                                               { left_elbow_frame_, left_shoulder_frame_ },
                                                               { right_hand_frame_, right_elbow_frame_ },
                                                               { left_hand_frame_, left_elbow_frame_ } };
 
-  std::map<std::string, std::string>::iterator string_map_iterator_;
+  std::map<std::string, std::pair<double, double>> joint_limits_map;
+  std::map<std::string, std::pair<double, double>>::iterator joint_limits_iterator_;
 
-  std::vector<std::string> frames_vector_ = {left_elbow_frame_, left_hand_frame_};
+  // DO NOT CHANGE THE SEQUENCE
+  std::string left_wrist_dummy = "left_dummy", right_wrist_dummy = "right_dummy";
+  std::vector<std::string> left_arm_names_, right_arm_names_, chest_names_;
+
+  std::vector<std::string> frames_vector_ = {
+    left_elbow_frame_,
+    left_hand_frame_,
+    left_wrist_dummy
+  };
 
   tf::TransformListener tf_listener_;
   tf::StampedTransform tf_transform_;
@@ -65,6 +75,9 @@ private:
   void getRollYaw(const tf::StampedTransform& transform, double& roll, double& yaw);
   tf::StampedTransform getTransform(const std::string& foot_frame, const std::string& ref_frame);
   void resizeJointTrajectory();
+  void updateJointLimits();
+  void insertJointLImits(const std::vector<std::string>& joint_names_vector);
+  void addToJointTrajectory(const std::string& frame_name, const std::string& rotation_frame, double rotation);
   // bool updateJointAngle(std::string frame_name, );
 
   void control();
--- src/shadow_upper_body.cpp
+++ src/shadow_upper_body.cpp
@@ -14,6 +14,29 @@ void ShadowUpperBody::initialize()
   rd_ = RobotDescription::getRobotDescription(nh_);
   robot_state_ = RobotStateInformer::getRobotStateInformer(nh_);
   wholebodyController_ = new WholebodyControlInterface(nh_);
+  updateJointLimits();
+}
+
+void ShadowUpperBody::updateJointLimits()
+{
+  rd_->getLeftArmJointNames(left_arm_names_);
+  insertJointLImits(left_arm_names_);
+
+  rd_->getRightArmJointNames(right_arm_names_);
+  insertJointLImits(right_arm_names_);
+  
+  rd_->getChestJointNames(chest_names_);
+  insertJointLImits(chest_names_);
+}
+
+void ShadowUpperBody::insertJointLImits(const std::vector<std::string>& joint_names_vector)
+{
+  std::vector <std::pair<double, double>> joint_limits_temp;
+  std::vector<std::string> joint_names_temp;
+
+  rd_->getLeftArmJointLimits(joint_limits_temp);
+  for (int i = 0; i < joint_names_vector.size(); i++)
+    joint_limits_map.insert(std::pair<std::string, std::pair<double, double>>(joint_names_vector.at(i), joint_limits_temp.at(i)));
 }
 
 void ShadowUpperBody::update()
@@ -25,6 +48,7 @@ void ShadowUpperBody::update()
 
 void ShadowUpperBody::clearJointTrajectory()
 {
+  joint_trajectory_.header = std_msgs::Header();
   joint_trajectory_.header.frame_id = rd_->getPelvisFrame();
   joint_trajectory_.header.seq = 1;
   joint_trajectory_.joint_names.clear();
@@ -51,9 +75,28 @@ void ShadowUpperBody::updateTranforms()
   tf::StampedTransform transform;
   for (auto i : frames_vector_)
   {
+    std::string frame_name = i.data();
+    if (frame_name.compare(left_wrist_dummy) == 0)
+    {
+      for (int index = 4; index < 7; index++)
+      {
+        joint_trajectory_.joint_names.push_back(left_arm_names_.at(index));
+        joint_trajectory_.points.front().positions.push_back(0);
+      }
+      continue;
+    }
+    else if(frame_name.compare(right_wrist_dummy)==0)
+    {
+      for (int index = 4; index < 7; index++)
+      {
+        joint_trajectory_.joint_names.push_back(right_arm_names_.at(index));
+        joint_trajectory_.points.front().positions.push_back(0);  
+      }
+      continue;
+    }
     transform = getTransform(i.data(), child_parent_frames_[i.data()]);
     getRollYaw(transform, roll, yaw);
-    updateJointTrajectoryMsg(i.data(), roll, yaw);    
+    updateJointTrajectoryMsg(child_parent_frames_[i.data()], roll, yaw);    
   }
 }
 
@@ -69,27 +112,36 @@ void ShadowUpperBody::updateJointTrajectoryMsg(const std::string& frame_name, do
   std::string yaw_frame = frame_name + yaw_;
   std::string roll_frame = frame_name + roll_;
   
-  string_map_iterator_ = human_robot_joint_map_.find(yaw_frame);
+  // DO NOT CHANGE THE SEQUENCE
+
+  addToJointTrajectory(frame_name, yaw_frame, yaw);
+  addToJointTrajectory(frame_name, roll_frame, roll);
+}
+
+void ShadowUpperBody::addToJointTrajectory(const std::string& frame_name, const std::string& rotation_frame, double rotation)
+{
+  std::pair<double, double> joint_limit_temp;
+
+  string_map_iterator_ = human_robot_joint_map_.find(rotation_frame);
   if(string_map_iterator_ != human_robot_joint_map_.end())
   {
-    joint_trajectory_.joint_names.push_back(human_robot_joint_map_[yaw_frame]);
-    joint_trajectory_.points.front().positions.push_back(yaw);
+  joint_limits_iterator_ = joint_limits_map.find(human_robot_joint_map_[rotation_frame]);
+  if(joint_limits_iterator_ != joint_limits_map.end())
+  {
+    joint_limit_temp = joint_limits_map[human_robot_joint_map_[rotation_frame]];
+    if (rotation < joint_limit_temp.first)
+      rotation = joint_limit_temp.first;
+    else if(rotation > joint_limit_temp.second)
+      rotation = joint_limit_temp.second;
+  }
+    joint_trajectory_.joint_names.push_back(human_robot_joint_map_[rotation_frame]);
+    joint_trajectory_.points.front().positions.push_back(rotation);
   }
   else
   {
-    ROS_ERROR("%s frame not found in map human_robot_joint_map_", yaw_frame.c_str());
+    ROS_ERROR("%s frame not found in map human_robot_joint_map_", rotation_frame.c_str());
   }
-  
-  string_map_iterator_ = human_robot_joint_map_.find(roll_frame);
-  if(string_map_iterator_ != human_robot_joint_map_.end())
-  {
-    joint_trajectory_.joint_names.push_back(human_robot_joint_map_[roll_frame]);
-    joint_trajectory_.points.front().positions.push_back(roll);
-  }
-  else
-  {
-    ROS_ERROR("%s frame not found in map human_robot_joint_map_", roll_frame);
-  }
+
 }
 
 tf::StampedTransform ShadowUpperBody::getTransform(const std::string& frame_name, const std::string& parent_frame)
@@ -121,6 +173,9 @@ void ShadowUpperBody::execute()
 
 void ShadowUpperBody::startShadowMotion()
 {
-  update();
-  execute();
+  while(run_code)
+  {
+    update();
+    execute();
+  }
 }
