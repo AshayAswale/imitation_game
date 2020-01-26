871faaf63ba9e5fa608e183da4620e11bdb273ea
Ashay Aswale <asaswale@wpi.edu>
2020-01-25 19:01:15 -0500

    Left leg graph plotting

--- src/tf_pub.cpp
+++ src/tf_pub.cpp
@@ -3,22 +3,24 @@
 #include <tf/transform_listener.h>
 #include <imitation_game/Joints.h>
 
-imitation_game::Eulers getTransformation(const std::string &track_frame, const std::string &ref_frame, const std::string &name, const bool elb_comp = false)
+geometry_msgs::Point getTransformation(const std::string &track_frame, const std::string &ref_frame, const std::string &name, const bool is_op = false)
 {
-  imitation_game::Eulers euler;
+  static geometry_msgs::Point point;
   static tf::TransformListener listener;
   static tf::StampedTransform transform;
   try
   {
     listener.waitForTransform(track_frame, ref_frame, ros::Time(0.0), ros::Duration(0.25));
     listener.lookupTransform(track_frame, ref_frame, ros::Time(0.0), transform);
-    tf::Matrix3x3 mat(transform.getRotation());
-    mat.getRPY(euler.roll, euler.pitch, euler.yaw);
+    // tf::Matrix3x3 mat(transform.getRotation());
+    // mat.getRPY(euler.roll, euler.pitch, euler.yaw);
 
-    if (elb_comp)
-      euler.roll = std::max(std::abs(euler.yaw), std::abs(euler.roll));
-
-    euler.name = name;
+    // if (elb_comp)
+    //   euler.roll = std::max(std::abs(euler.yaw), std::abs(euler.roll));
+    float scale = is_op ? -0.83 : 1;
+    point.x = transform.getOrigin().getX() * scale;
+    point.y = transform.getOrigin().getY() * scale;
+    point.z = transform.getOrigin().getZ() * scale;
   }
   catch (const std::exception &e)
   {
@@ -31,9 +33,9 @@ imitation_game::Eulers getTransformation(const std::string &track_frame, const s
       getTransformation(track_frame, ref_frame, name);
     }
     fail = 0;
-    euler.name = "FAILED!!!";
+    // euler.name = "FAILED!!!";
   }
-  return euler;
+  return point;
 }
 
 void normalizeEuler(imitation_game::Joints &joints)
@@ -47,22 +49,22 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "tf_pub");
   ros::NodeHandle nh;
-  ros::Rate rate(1000);
+  ros::Rate rate(500);
   ros::AsyncSpinner spin(1);
   spin.start();
 
-  ros::Publisher publisher = nh.advertise<imitation_game::Joints>("elbow_pose", 1000, true);
-  imitation_game::Joints all_joints;
-  all_joints.joints.resize(4);
+  ros::Publisher op_publisher = nh.advertise<geometry_msgs::Point>("operator_leg_pose", 10, true);
+  ros::Publisher rob_publisher = nh.advertise<geometry_msgs::Point>("robot_leg_pose", 10, true);
+  geometry_msgs::Point operator_leg, robot_leg;
+
   while (ros::ok())
   {
-    all_joints.joints.at(0) = getTransformation("/openni/left_elbow", "/openni/left_shoulder", "left_shoulder");
-    all_joints.joints.at(1) = getTransformation("/openni/left_hand", "/openni/left_elbow", "left_elbow", true);
-    all_joints.joints.at(2) = getTransformation("/openni/right_elbow", "/openni/right_shoulder", "right_shoulder");
-    all_joints.joints.at(3) = getTransformation("/openni/right_hand", "/openni/right_elbow", "right_elbow");
-    normalizeEuler(all_joints);
+    operator_leg = getTransformation("/openni/left_foot", "/openni/right_foot", "operator_left_leg", true);
+    robot_leg = getTransformation("/l_foot", "/r_foot", "robot_left_leg");
 
-    publisher.publish(all_joints);
+    operator_leg.x *= -1;
+    op_publisher.publish(operator_leg);
+    rob_publisher.publish(robot_leg);
     rate.sleep();
   }
   spin.stop();
